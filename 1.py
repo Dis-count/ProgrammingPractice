b9d1cf2a8f3cae5794c75835a478b16a5dbe6e3a
Discount <492193947@qq.com>
2022-08-08 22:59:20 +0800

    Update

--- 1.py
+++ 1.py
@@ -2,5 +2,8 @@
 #  这一串数字 是 非递减数列  满足就输出 true 不满足就输出 false
 #  例如 3 4 1 2  不可以通过 修改 4 -> 3 而得到 非递减数列
 # 因而可以判断  至多只有一个波段 才可以满足修改一个数字得到非递减数列
+from matplotlib import pyplot as plt
+
+
 num[i] \geq num[i+1] and num[i-1] \geq num[i+1]
-超过一个起伏  即可判断 不满足
+# 超过一个起伏  即可判断 不满足
---
+++ Java/q2/q2.java
@@ -0,0 +1,9 @@
+/**
+ * q2
+ */
+public class q2 {
+
+    public static void main(String[] args) {
+        System.out.println("what");
+    }
+}
